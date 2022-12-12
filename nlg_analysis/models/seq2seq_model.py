783ef66b28da3cc569c44de676ad55264c1b9b74
wwolny <wwolny1@gmail.com>
2022-12-12 19:12:40 +0100

    add seq2seq transcript generate

--- nlg_analysis/models/seq2seq_model.py
+++ nlg_analysis/models/seq2seq_model.py
@@ -7,17 +7,64 @@ from torch import nn, optim
 from nlg_analysis.models.base_model import BaseModel
 from nlg_analysis.models.seq2seq_decoder import Seq2SeqDecoder
 from nlg_analysis.models.seq2seq_encoder import Seq2SeqEncoder
-from nlg_analysis.models.seq2seq_utils import prepareData, tensorsFromPair
+from nlg_analysis.models.seq2seq_utils import (
+    prepareData,
+    tensorFromSentence,
+    tensorsFromPair,
+)
 
 
 class Seq2SeqModel(BaseModel):
-    def __init__(self):
+    def __init__(
+        self,
+        questions_path: str,
+        answers_path: str,
+        path2encoder: str = None,
+        path2decoder: str = None,
+    ):
         super(Seq2SeqModel, self).__init__()
         self.sos_token = 0
         self.eos_token = 1
+        self.questions_path = questions_path
+        self.answers_path = answers_path
+        if path2encoder is not None:
+            self.path2encoder = path2encoder
+        else:
+            self.path2encoder = None
+        if path2decoder is not None:
+            self.path2decoder = path2decoder
+        else:
+            self.path2decoder = None
 
     def generate_transcript(self, questions: List) -> str:
-        pass
+        if self.path2decoder is None and self.path2encoder is None:
+            raise ValueError
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        hidden_size = 256
+        question_side, answer_side, pairs = prepareData(
+            self.questions_path, self.answers_path
+        )
+        encoder = Seq2SeqEncoder(
+            question_side.n_words, hidden_size, device=device
+        )
+        encoder.load_state_dict(torch.load(self.path2encoder))
+        decoder = Seq2SeqDecoder(
+            hidden_size, answer_side.n_words, dropout_p=0.1, device=device
+        )
+        decoder.load_state_dict(torch.load(self.path2decoder))
+
+        trans_txt = ""
+        for q in questions:
+            output_words, attentions = self.evaluate(
+                encoder=encoder,
+                decoder=decoder,
+                sentence=q,
+                device=device,
+                answer_side=answer_side,
+                question_side=question_side,
+            )
+            trans_txt += q + "\n" + " ".join(output_words[:-1]) + "\n"
+        return trans_txt
 
     @staticmethod
     def approach() -> str:
@@ -25,8 +72,6 @@ class Seq2SeqModel(BaseModel):
 
     def train(
         self,
-        questions_path: str,
-        answers_path: str,
         encoder_output_path: str,
         decoder_output_path: str,
     ):
@@ -35,7 +80,7 @@ class Seq2SeqModel(BaseModel):
         hidden_size = 256
         n_iters = 75000
         input_lang, output_lang, pairs = prepareData(
-            questions_path, answers_path
+            self.questions_path, self.answers_path
         )
         encoder = Seq2SeqEncoder(input_lang.n_words, hidden_size, device).to(
             device
@@ -151,3 +196,56 @@ class Seq2SeqModel(BaseModel):
         decoder_optimizer.step()
 
         return loss.item() / target_length
+
+    def evaluate(
+        self,
+        encoder,
+        decoder,
+        sentence,
+        question_side,
+        answer_side,
+        device,
+        max_length=32,
+    ):
+        with torch.no_grad():
+            input_tensor = tensorFromSentence(
+                question_side,
+                sentence,
+                eos_token=self.eos_token,
+                device=device,
+            )
+            input_length = input_tensor.size()[0]
+            encoder_hidden = encoder.initHidden()
+
+            encoder_outputs = torch.zeros(
+                max_length, encoder.hidden_size, device=device
+            )
+
+            for ei in range(input_length):
+                encoder_output, encoder_hidden = encoder(
+                    input_tensor[ei], encoder_hidden
+                )
+                encoder_outputs[ei] += encoder_output[0, 0]
+
+            decoder_input = torch.tensor([[self.sos_token]], device=device)
+
+            decoder_hidden = encoder_hidden
+
+            decoded_words = []
+            decoder_attentions = torch.zeros(max_length, max_length)
+
+            for di in range(max_length):
+                decoder_output, decoder_hidden, decoder_attention = decoder(
+                    decoder_input, decoder_hidden, encoder_outputs
+                )
+                decoder_attentions[di] = decoder_attention.data
+                topv, topi = decoder_output.data.topk(1)
+                if topi.item() == self.eos_token:
+                    decoded_words.append("<EOS>")
+                    break
+                else:
+                    decoded_words.append(answer_side.index2word[topi.item()])
+
+                decoder_input = topi.squeeze().detach()
+
+            return decoded_words, decoder_attentions[: di + 1]
