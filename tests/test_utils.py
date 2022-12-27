91f80d127b5f8f5eab51e0f25205f03f7788c1f6
wwolny <wwolny1@gmail.com>
2022-12-27 14:50:53 +0100

    add utils test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -17,3 +17,15 @@ def test_parse_args_long():
     new_args = ["--config-file", path_to_file]
     parser = parse_arguments(new_args)
     assert parser.config_file == path_to_file
+
+
+def test_load_questions():
+    path_to_file = "tests/resources/sample_questions.csv"
+    df_q = load_questions(path_to_file)
+    assert df_q.shape == (2, 8)
+
+
+def test_load_questions_header():
+    path_to_file = "tests/resources/sample_questions.csv"
+    df_q = load_questions(path_to_file, header=None)
+    assert df_q.shape == (3, 8)
